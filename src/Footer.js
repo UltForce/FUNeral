c2d3988133c08adc1d5e7fb85aa22e79529fec09
divineblanco <divine.blanco.cics@ust.edu.ph>
2024-11-03 18:27:14 +0800

    fixed front end of footer, login, booking, faqs, register, terms, and styles

--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,38 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import './styles.css';
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <OverlayTrigger
+        <div className="first-footer-content">
+          <div className="jroa-address">
+            <p><strong>Address:</strong>
+            <br/>
+            64 K4th kamuning, Quezon City, Philippines</p>
+          </div>
+          <br/>
+          <div className="contact-info">
+            <p><strong>Contact us: </strong>
+            <br/>
+            0909 081 3396 / 0935 354 4006</p>
+          </div>
+        </div>
+        <div className="second-footer-content">
+          <div className="operation-hours">
+            <p><strong>Operation Hours:</strong>
+            <br/>
+            Temoporary Text
+            </p>
+          </div>
+
+          <OverlayTrigger
           placement="bottom"
           overlay={<Tooltip>Visit our Facebook page</Tooltip>}
-        >
+          >
           <div className="social-media">
+            <p><strong>Get in Touch</strong></p>
             <a
               href="https://www.facebook.com/profile.php?id=61556915982294"
               target="_blank"
@@ -22,11 +45,10 @@ const Footer = () => {
             <br />
           </div>
         </OverlayTrigger>
-        <div className="contact-info">
-          <p>Contact us: 0909 081 3396 / 0935 354 4006</p>
         </div>
+        
       </div>
-      <p>&copy; 2024 J.ROA Funeral Services. All rights reserved.</p>
+      <p className="jroa-copyright">&copy; 2024 J.ROA Funeral Services. All rights reserved.</p>
     </footer>
   );
 };
