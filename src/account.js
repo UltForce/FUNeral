8e00ecd02b5f9df82f26a24a39244239a03aaabd
divineblanco <divine.blanco.cics@ust.edu.ph>
2024-11-27 23:44:28 +0800

    capitalized and added space in the account table label

--- src/account.js
+++ src/account.js
@@ -325,7 +325,103 @@ const Account = () => {
                       <th className="account-table-label">Email:</th>
                       <td className="user-account-email">{user.email}</td>
                     </tr>
-                    {[
+                    <th className="account-table-label">Mobile Number:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="mobilenumber"
+                    value={editableData.mobilenumber || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              <tr>
+                <th className="account-table-label">Landline Number:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="landlinenumber"
+                    value={editableData.landlinenumber || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th className="account-table-label">Region:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="region"
+                    value={editableData.region || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th className="account-table-label">City:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="city"
+                    value={editableData.city || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th className="account-table-label">Barangay:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="barangay"
+                    value={editableData.barangay || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th className="account-table-label">Street:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="street"
+                    value={editableData.street || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th className="account-table-label">Unit:</th>
+                <td>
+                  <input
+                    type="text"
+                    name="unit"
+                    value={editableData.unit || ""}
+                    className="account-table-info"
+                    onChange={handleEditChange}
+                    style={{ width: "100%" }}
+                    readOnly={!editMode} // Set to readonly based on edit mode
+                  />
+                </td>
+              </tr>
+                    {/* {[
                       "mobilenumber",
                       "landlinenumber",
                       "region",
@@ -350,7 +446,7 @@ const Account = () => {
                           />
                         </td>
                       </tr>
-                    ))}
+                    ))} */}
                   </tbody>
                 </table>
                 <div className="account-buttons">
