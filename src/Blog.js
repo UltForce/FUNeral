62defcab835d13567f866e75993873c0de978f0e
reaaaaaaa03 <nicoleandrea.bandianon.cics@ust.edu.ph>
2024-12-03 02:57:02 +0800

    HELPSPSS

--- src/Blog.js
+++ src/Blog.js
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from "react";
 
 const Blog = () => {
   
-    
+   
 
 return (
     
@@ -24,7 +24,7 @@ return (
                     <h1>Filipino Beliefs You Should Respect When Attending Wakes</h1>
                     <p>Attending wakes in the Philippines often involves observing cultural practices rooted in deep respect for the departed and their families. 
                         Filipinos place significant importance on honoring their loved ones through traditions thatbring comfort and support to the grieving family. 
-                        Here are some beliefs andpractices you should be mindful of:
+                        Here are some beliefs andpractices you should be mindful of:  <p><a href="/blogpage">Read More!</a></p>
                     </p>
                 </div>
             </div>
---
+++ src/blogpage.js
@@ -0,0 +1,33 @@
+import "./blogpage.css";
+import React, { useEffect, useState } from "react";
+
+const blogpage = () => {
+
+    return(
+        <main className="main-content">
+    <section className="blog_pages">
+      <div>
+        <h1 className="blog-title">Filipino Beliefs You Should Respect When Attending Wakes</h1>
+        <div className="blog-border"></div>
+      </div>
+    </section>
+    <section className="blog-section">
+        <div className="blog-container">
+            <div className="blog-box">
+                <div className="blog-details">
+                    <h1>Filipino Beliefs You Should Respect When Attending Wakes</h1>
+                    <p>Attending wakes in the Philippines often involves observing cultural practices rooted in deep respect for the departed and their families. 
+                        Filipinos place significant importance on honoring their loved ones through traditions thatbring comfort and support to the grieving family. 
+                        Here are some beliefs andpractices you should be mindful of:  <p><a href="https://heritagememorialpark.com.ph/filipino-beliefs-you-should-respect/">Read More!</a></p>
+                    </p>
+                </div>
+            </div>
+
+           
+        </div>
+    </section>
+    </main>       
+    );
+};
+
+export default blogpage;
