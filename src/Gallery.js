d1ec8047e3b99e0bc7c33111120da127c9c02fcc
Fritz Cabe <fritzgerald.cabe.cics@ust.edu.ph>
2024-12-07 19:03:07 +0800

    fixed gallery images

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,8 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const Gallery = () => {
-  
-  
   const navigate = useNavigate(); // Initialize navigate function
 
   // Sample album data with placeholder funeral-related images
@@ -29,35 +27,33 @@ const Gallery = () => {
         "/gallery/funeral/fun_gallery (12).jpg",
         "/gallery/funeral/fun_gallery (13).jpg",
         "/gallery/funeral/fun_gallery (14).jpg",
-        
       ],
     },
     {
       id: 2,
       title: "Flower Arrangements",
-      coverImage: "/gallery/flowerarrangement/flower_gallery(1).jpg",
+      coverImage: "/gallery/flower arrangement/flower_gallery (1).jpg",
       images: [
-        "/gallery/flower arrangement/flower_gallery(1).jpg",
-        "/gallery/flower arrangement/flower_gallery(2).jpg",
-        "/gallery/flower arrangement/flower_gallery(3).jpg",
-        "/gallery/flower arrangement/flower_gallery(4).jpg",
-        "/gallery/flower arrangement/flower_gallery(5).jpg",
-        "/gallery/flower arrangement/flower_gallery(6).jpg",
-        
+        "/gallery/flower arrangement/flower_gallery (1).jpg",
+        "/gallery/flower arrangement/flower_gallery (2).jpg",
+        "/gallery/flower arrangement/flower_gallery (3).jpg",
+        "/gallery/flower arrangement/flower_gallery (4).jpg",
+        "/gallery/flower arrangement/flower_gallery (5).jpg",
+        "/gallery/flower arrangement/flower_gallery (6).jpg",
       ],
     },
     {
       id: 3,
       title: "Lights and Cars",
-      coverImage: "/gallery/lightsandcars/ lights(1).jpg",
+      coverImage: "/gallery/lights and cars/lights (1).jpg",
       images: [
-        "/gallery/lights and cars/ lights(1).jpg",
-        "/gallery/lights and cars/ lights(2).jpg",
-        "/gallery/lights and cars/ lights(3).jpg",
-        "/gallery/lights and cars/ lights(4).jpg",
-        "/gallery/lights and cars/ lights(5).jpg",
-        "/gallery/lights and cars/ car(1).jpg",
-        "/gallery/lights and cars/ car(2).jpg",
+        "/gallery/lights and cars/lights (1).jpg",
+        "/gallery/lights and cars/lights (2).jpg",
+        "/gallery/lights and cars/lights (3).jpg",
+        "/gallery/lights and cars/lights (4).jpg",
+        "/gallery/lights and cars/lights (5).jpg",
+        "/gallery/lights and cars/car (1).jpg",
+        "/gallery/lights and cars/car (2).jpg",
       ],
     },
   ];
@@ -97,89 +93,89 @@ const Gallery = () => {
 
   return (
     <main className="main-content">
-    <section className="gallery">
-      <div>
-        <h1 className="gallery-title">GALLERY</h1>
-        <div className="gallery-border"></div>
-      </div>
-    </section>
-    <section className="gallery-container">
-      {!selectedAlbum ? (
-        <div className="album-selector">
-          {albums.map((album) => (
-            <OverlayTrigger
-              key={album.id} // Key is now on OverlayTrigger
-              placement="right"
-              overlay={<Tooltip>View Album</Tooltip>}
-            >
-              <div
-                className="album-cover"
-                onClick={() => handleAlbumClick(album)}
-              >
-                <img
-                  src={album.coverImage}
-                  alt={album.title}
-                  className="album-image"
-                />
-                <p className="album-title">{album.title}</p>
-              </div>
-            </OverlayTrigger>
-          ))}
+      <section className="gallery">
+        <div>
+          <h1 className="gallery-title">GALLERY</h1>
+          <div className="gallery-border"></div>
         </div>
-      ) : (
-        <div className="album-preview">
-          <div className="preview-container">
-            <img src={previewImage} alt="Preview" className="preview-image" />
-          </div>
-
-          <div className="thumbnail-pagination">
-            <button
-              className="pagination-button"
-              onClick={handlePrevious}
-              disabled={thumbnailIndex === 0}
-            >
-              {"<"}
-            </button>
-
-            <div className="thumbnail-container">
-              {selectedAlbum.images
-                .slice(thumbnailIndex, thumbnailIndex + thumbnailsPerPage)
-                .map((image) => (
+      </section>
+      <section className="gallery-container">
+        {!selectedAlbum ? (
+          <div className="album-selector">
+            {albums.map((album) => (
+              <OverlayTrigger
+                key={album.id} // Key is now on OverlayTrigger
+                placement="right"
+                overlay={<Tooltip>View Album</Tooltip>}
+              >
+                <div
+                  className="album-cover"
+                  onClick={() => handleAlbumClick(album)}
+                >
                   <img
-                    key={image} // Use `image` URL as the key
-                    src={image}
-                    alt="Thumbnail"
-                    className={`thumbnail ${
-                      previewImage === image ? "active" : ""
-                    }`}
-                    onClick={() => handleThumbnailClick(image)}
+                    src={album.coverImage}
+                    alt={album.title}
+                    className="album-image"
                   />
-                ))}
+                  <p className="album-title">{album.title}</p>
+                </div>
+              </OverlayTrigger>
+            ))}
+          </div>
+        ) : (
+          <div className="album-preview">
+            <div className="preview-container">
+              <img src={previewImage} alt="Preview" className="preview-image" />
             </div>
 
-            <button
-              className="pagination-button"
-              onClick={handleNext}
-              disabled={
-                thumbnailIndex + thumbnailsPerPage >=
-                selectedAlbum.images.length
-              }
-            >
-              {">"}
-            </button>
-          </div>
+            <div className="thumbnail-pagination">
+              <button
+                className="pagination-button"
+                onClick={handlePrevious}
+                disabled={thumbnailIndex === 0}
+              >
+                {"<"}
+              </button>
 
-          <OverlayTrigger
-            placement="right"
-            overlay={<Tooltip>Return to Album View</Tooltip>}
-          >
-            <button className="return-button" onClick={returnToGallery}>
-              Return to Album Gallery
-            </button>
-          </OverlayTrigger>
-        </div>
-      )}
-    </section>
+              <div className="thumbnail-container">
+                {selectedAlbum.images
+                  .slice(thumbnailIndex, thumbnailIndex + thumbnailsPerPage)
+                  .map((image) => (
+                    <img
+                      key={image} // Use `image` URL as the key
+                      src={image}
+                      alt="Thumbnail"
+                      className={`thumbnail ${
+                        previewImage === image ? "active" : ""
+                      }`}
+                      onClick={() => handleThumbnailClick(image)}
+                    />
+                  ))}
+              </div>
+
+              <button
+                className="pagination-button"
+                onClick={handleNext}
+                disabled={
+                  thumbnailIndex + thumbnailsPerPage >=
+                  selectedAlbum.images.length
+                }
+              >
+                {">"}
+              </button>
+            </div>
+
+            <OverlayTrigger
+              placement="right"
+              overlay={<Tooltip>Return to Album View</Tooltip>}
+            >
+              <button className="return-button" onClick={returnToGallery}>
+                Return to Album Gallery
+              </button>
+            </OverlayTrigger>
+          </div>
+        )}
+      </section>
     </main>
   );
 };
